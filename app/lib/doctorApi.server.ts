7478f24af0de8b79064a75ec4591c330dd1f4c51
h-malaviya <harshil.m@bitontree.com>
2026-02-10 16:58:24 +0530

    fix : Fix doctor list loading (#6)

--- app/lib/doctorApi.server.ts
+++ app/lib/doctorApi.server.ts
@@ -7,7 +7,9 @@ import { DoctorsApiParams } from "@/app/components/doctor/types"
 export async function fetchDoctorsFromServer(params: DoctorsApiParams) {
   try {
     const query = buildQueryString(params)
-    const data = await serverFetch(`doctors?${query}`)
+    const data = await serverFetch(`doctors?${query}`,{
+      cache: "no-store",
+    })
 
     const doctorsArray = Array.isArray(data.doctors)
     ? data.doctors
